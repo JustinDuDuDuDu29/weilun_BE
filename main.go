fab911d0709aeec572c16170872086c80a341252
Justin <justinduchengkai@gmail.com>
2024-06-21 13:24:24 +0800

    change some ins

--- main.go
+++ main.go
@@ -5,30 +5,28 @@ import (
 	"main/router"
 	"main/utils"
 	"main/wiregen"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	ticker := time.NewTicker(5000 * time.Millisecond)
-	go func() {
-		for range ticker.C {
-			res, err := http.Get("https://www.imdu29.com")
-			fmt.Println(res.StatusCode)
-			if res.StatusCode == 404 {
-				fmt.Println("404!")
-				ticker.Stop()
-				return
-			}
-			if res.StatusCode != 200 || err != nil {
-				fmt.Println("not 200!")
-				os.Exit(1)
-			}
-		}
-	}()
+	// ticker := time.NewTicker(5000 * time.Millisecond)
+	// go func() {
+	// 	for range ticker.C {
+	// 		res, err := http.Get("https://www.imdu29.com")
+	// 		fmt.Println(res.StatusCode)
+	// 		if res.StatusCode == 404 {
+	// 			fmt.Println("404!")
+	// 			ticker.Stop()
+	// 			return
+	// 		}
+	// 		if res.StatusCode != 200 || err != nil {
+	// 			fmt.Println("not 200!")
+	// 			os.Exit(1)
+	// 		}
+	// 	}
+	// }()
 	fmt.Println("Starting version: ", os.Getenv("version"))
 
 	godotenv.Load()
